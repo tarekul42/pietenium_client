15603972530c93af96d8f3d5bca85ddf948dc2f3
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-05-19 23:49:33 +0600

    #Update1.39 our works start

---
+++ src/app/our-works/page.jsx
@@ -0,0 +1,8 @@
+import OurWorks from '@/components/works/OurWorks';
+import React from 'react';
+
+const page = () => {
+    return <OurWorks/>;
+};
+
+export default page;
---
+++ src/components/works/OurWorks.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OurWorks = () => {
+    return (
+        <div>
+            <h1>Worsks</h1>
+        </div>
+    );
+};
+
+export default OurWorks;
